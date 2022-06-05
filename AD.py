d5bdcf60796f827d59345a2c68f7f406105e1883
Abdelfetah MAAMRA <44534871+lorenzcipher@users.noreply.github.com>
2022-06-06 00:27:22 +0100

    Update AD.py

--- AD.py
+++ AD.py
@@ -19,7 +19,7 @@ from streamlit_option_menu import option_menu
 import streamlit.components.v1 as html
 from  PIL import Image
 import numpy as np
-import cv2
+
 import pandas as pd
 from st_aggrid import AgGrid
 import plotly.express as px
